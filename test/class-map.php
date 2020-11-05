5689e78048d4a40200d8f622f48cc187937a10cd
Michael Uno <michael2@michaeluno.jp>
2020-11-06 01:25:16 +0900

    [Test] Generate a test class map.

--- test/class-map.php
+++ test/class-map.php
@@ -1,5 +1,5 @@
 <?php 
-return array( 
+return array(
     "Foo\\FooClass" => CLASS_MAP_BASE_DIR_VAR . "/_scandir/FooClass.php", 
     "Foo\\FooClass_Base" => CLASS_MAP_BASE_DIR_VAR . "/_scandir/FooClass_Base.php", 
     "NoNamespace" => CLASS_MAP_BASE_DIR_VAR . "/_scandir/NoNamespace.php", 
