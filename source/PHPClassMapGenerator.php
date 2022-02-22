21a5af880fbf68079ea3ea91ac58f08c0694357f
Michael Uno <michael2@michaeluno.jp>
2022-02-22 20:05:36 +0900

    Fix: Revert the default value of the `base_dir_var` argument
    The previous change was made in `10a508e3`.

--- source/PHPClassMapGenerator.php
+++ source/PHPClassMapGenerator.php
@@ -109,7 +109,7 @@ class PHPClassMapGenerator implements interfacePHPClassMapGenerator {
             // 'Foo/Bar' // for name spaced classes, include the name space
         ],
 
-        'base_dir_var'          => '__DIR__',
+        'base_dir_var'          => 'CLASS_MAP_BASE_DIR_VAR',
         'output_var_name'       => '$aClassMap',
         'structure'             => 'CLASS',     // 1.1.0 Accepted values: `CLASS`, `PATH` For `CLASS`, the generated array keys consist of class names. For `PATH` array keys will consist of file paths.
         'do_in_constructor'     => true,        // 1.1.0 Whether to perform the task in the constructor
